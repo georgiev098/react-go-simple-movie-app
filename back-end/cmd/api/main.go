264d14d3a69edb89f600e9552369ddae409ce789
Daniel Georgiev <georgiev.daniel@outlook.com>
2025-08-17 18:12:32 +0300

    add a postgres connection

--- back-end/cmd/api/main.go
+++ back-end/cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,15 +12,26 @@ const port = 8080
 
 type application struct {
 	Domain string
+	DSN    string
+	DB     *sql.DB
 }
 
 func main() {
 	var app application
 	app.Domain = "example.com"
 
+	flag.StringVar(&app.DSN, "dsn", "host=host.docker.internal port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres Connection String")
+	flag.Parse()
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = conn
+
 	log.Println("Starting server on port:", port)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
